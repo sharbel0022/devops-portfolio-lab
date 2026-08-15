69cd5ca359ece14f84a0e69027338905b3985004
Sharbel Rasho <din@email.com>
2026-08-15 09:28:21 +0200

    feat: add tasks API endpoint

--- app/app.js
+++ app/app.js
@@ -4,16 +4,35 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
+const tasks = [
+  {
+    id: 1,
+    title: "Learn Git",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Learn Docker",
+    completed: false,
+  },
+];
+
 app.get("/", (req, res) => {
-    res.send("NordicByte DevOps Platform");
+  res.send("NordicByte DevOps Platform");
 });
 
 app.get("/health", (req, res) => {
-    res.json({
-        status: "healthy"
-    });
+  res.json({
+    status: "healthy",
+  });
+});
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
 });
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
